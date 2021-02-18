7edd12ec9bcb9846f6b7f541435164243f96a9d0
Andy Wijaya <andy@happymama.id>
2021-02-18 15:55:58 +0700

    done local config

--- config/config.go
+++ config/config.go
@@ -2,20 +2,61 @@ package config
 
 import (
 	"github.com/spf13/viper"
-	"log"
 )
 
 var config = viper.New()
 
-func InitConfig () {
-	config.SetConfigName("config")
-	config.AddConfigPath(".")
+func InitConfig (filename string, path string) error {
+	config.SetConfigName(filename)
+	config.AddConfigPath(path)
 	err := config.ReadInConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return err
+}
+
+func WatchConfig() {
+	config.WatchConfig()
+}
+
+func Get(key string) interface{} {
+	return config.Get(key)
+}
+
+func GetBool (key string) bool {
+	return config.GetBool(key)
+}
+
+func GetInt (key string) int {
+	return config.GetInt(key)
+}
+
+func GetInt32 (key string) int32{
+	return config.GetInt32(key)
+}
+
+func GetInt64 (key string) int64 {
+	return config.GetInt64(key)
+}
+
+func GetIntSlice (key string) []int {
+	return config.GetIntSlice(key)
 }
 
 func GetString (key string) string {
 	return config.GetString(key)
+}
+
+func GetStringSlice (key string) []string {
+	return config.GetStringSlice(key)
+}
+
+func GetStringMap (key string) map[string]interface{} {
+	return config.GetStringMap(key)
+}
+
+func GetStringMapString (key string) map[string]string {
+	return config.GetStringMapString(key)
+}
+
+func GetStringMapStringSlice (key string) map[string][]string {
+	return config.GetStringMapStringSlice(key)
 }
