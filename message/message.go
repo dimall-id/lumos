5ed0d8dd8ee595254e2ca365eb7d26787dc2d3b3
Andy Wijaya <andy@happymama.id>
2021-06-11 12:03:30 +0700

    add send message to send to multiple destination

--- message/message.go
+++ message/message.go
@@ -1,9 +1,37 @@
 package message
 
-type Message struct {
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/dimall-id/lumos/v2/errs"
+	"github.com/dimall-id/lumos/v2/event"
+	"gorm.io/gorm"
+)
+
+type SendTo struct {
 	SendTo string `json:"send_to"`
 	SendType string `json:"send_type"`
+}
+
+type Message struct {
+	SendTo []string `json:"send_to"`
 	Subject string `json:"subject"`
 	Template string `json:"template"`
 	Data map[string]interface{} `json:"data"`
+}
+
+func SendMessageCommand (message Message, tx *gorm.DB) error {
+	jsonRes, err := json.Marshal(message)
+	var res bytes.Buffer
+	err = json.Compact(&res, jsonRes)
+	if err != nil {return errs.DecodeJsonErr(err)}
+	msg := event.LumosMessage{
+		Topic: "SEND_MESSAGE_COMMAND",
+		Key: "DATA",
+		Value: res.String(),
+		Headers: map[string]string{},
+	}
+	err = event.GenerateOutbox(tx, msg)
+	if err != nil {return errs.GenerateOutboxErr(err)}
+	return nil
 }
