db06a01e37028ed47ab46c5208fb4cadde7363fe
Andy Wijaya <zeno@Andys-MacBook-Pro.local>
2022-03-29 20:39:43 +0700

    reformat message name

--- message/message.go
+++ message/message.go
@@ -8,31 +8,37 @@ import (
 	"gorm.io/gorm"
 )
 
-type SendTo struct {
-	SendTo string `json:"send_to"`
-	SendType string `json:"send_type"`
+type Destination struct {
+	Address string `json:"address"`
+	Type    string `json:"type"`
 }
 
 type Message struct {
-	SendTo      []SendTo               `json:"send_to"`
+	// Origin use to state the name of the sender. this only used for email.
+	Origin      string                 `json:"origin"`
+	Destination Destination            `json:"destination"`
 	Subject     string                 `json:"subject"`
 	Template    string                 `json:"template"`
 	CountryCode string                 `json:"country_code"`
 	Data        map[string]interface{} `json:"data"`
 }
 
-func SendMessageCommand (message Message, tx *gorm.DB) error {
+func SendMessageCommand(message Message, tx *gorm.DB) error {
 	jsonRes, err := json.Marshal(message)
 	var res bytes.Buffer
 	err = json.Compact(&res, jsonRes)
-	if err != nil {return errs.DecodeJsonErr(err)}
+	if err != nil {
+		return errs.DecodeJsonErr(err)
+	}
 	msg := event.LumosMessage{
-		Topic: "SEND_MESSAGE_COMMAND",
-		Key: "DATA",
-		Value: res.String(),
+		Topic:   "SEND_MESSAGE_COMMAND",
+		Key:     "DATA",
+		Value:   res.String(),
 		Headers: map[string]string{},
 	}
 	err = event.GenerateOutbox(tx, msg)
-	if err != nil {return errs.GenerateOutboxErr(err)}
+	if err != nil {
+		return errs.GenerateOutboxErr(err)
+	}
 	return nil
 }
